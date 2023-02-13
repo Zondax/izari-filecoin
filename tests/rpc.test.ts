b462c66b8d8768cea04793e63e837571b4a32fbb
Emmanuel <emmanuelm41@gmail.com>
2023-02-13 10:36:58 -0300

    feat: add test for estimate fees

--- tests/rpc.test.ts
+++ tests/rpc.test.ts
@@ -5,6 +5,18 @@ const nodeToken = process.env.NODE_RPC_TOKEN
 if (!nodeUrl) throw new Error('NODE_RPC_URL must be defined')
 if (!nodeToken) throw new Error('NODE_RPC_TOKEN must be defined')
 
+const TX_TEMPLATE = {
+  From: 'REPLACE-ME',
+  To: 'REPLACE-ME',
+  Value: 'REPLACE-ME',
+  Method: 0,
+  Params: '',
+  Nonce: 'REPLACE-ME',
+  GasFeeCap: '0',
+  GasPremium: '0',
+  GasLimit: 0,
+}
+
 describe('Filecoin RPC', () => {
   test('Get nonce for new account', async () => {
     const rpcNode = new RPC({ url: nodeUrl, token: nodeToken })
@@ -34,11 +46,30 @@ describe('Filecoin RPC', () => {
     )
   })
 
-  test('Estimate fees for send tx', () => {
-    // TODO
+  test('Estimate fees for send tx', async () => {
+    const address = 'f410fr5lrinnngtqxl36rqvf6ykjm6tkmqi44ehqpybi'
+
+    const rpcNode = new RPC({ url: nodeUrl, token: nodeToken })
+
+    const response = await rpcNode.getNonce(address)
+    expect('error' in response).toBe(false)
+    if ('error' in response) return
+
+    let tx = { ...TX_TEMPLATE, From: address, To: address, Nonce: response.result, Value: '100000' }
+    const fees = await rpcNode.getGasEstimation({ ...tx })
+
+    expect('error' in fees).toBe(false)
+    expect('result' in fees).toBe(true)
+    if ('result' in fees) {
+      const { GasFeeCap, GasPremium, GasLimit } = fees.result
+      expect(GasFeeCap).toBeDefined()
+      expect(GasFeeCap).toBeDefined()
+      expect(GasFeeCap).toBeDefined()
+    }
   })
 
   test('Broadcast new transaction', () => {
     // TODO
+    // Need to sign a message first in order to test this
   })
 })
