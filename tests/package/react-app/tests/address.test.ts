e607f33d9d2061ae5e94961fdee8d02d9c85a09b
Emmanuel <emmanuelm41@gmail.com>
2023-02-16 12:51:23 -0300

    fix: close browser after test

--- tests/package/react-app/tests/address.test.ts
+++ tests/package/react-app/tests/address.test.ts
@@ -15,21 +15,23 @@ describe('Addresses', () => {
       userAgent: '',
     })
 
-    await page.goto('http://localhost:3000/')
-    await page.waitForSelector('#network')
+    try {
+      await page.goto('http://localhost:3000/')
+      await page.waitForSelector('#network', { timeout: 10000 })
 
-    const network = await page.$eval('#network', e => e.innerHTML)
-    const protocol = await page.$eval('#protocol', e => e.innerHTML)
-    const payload = await page.$eval('#payload', e => e.innerHTML)
-    const string = await page.$eval('#string', e => e.innerHTML)
-    const bytes = await page.$eval('#bytes', e => e.innerHTML)
+      const network = await page.$eval('#network', e => e.innerHTML)
+      const protocol = await page.$eval('#protocol', e => e.innerHTML)
+      const payload = await page.$eval('#payload', e => e.innerHTML)
+      const string = await page.$eval('#string', e => e.innerHTML)
+      const bytes = await page.$eval('#bytes', e => e.innerHTML)
 
-    expect(network).toBe('Network: t')
-    expect(payload).toBe('Payload: da43')
-    expect(protocol).toBe('Protocol: 0')
-    expect(string).toBe('String: t08666')
-    expect(bytes).toBe('Bytes: 00da43')
-
-    await browser.close()
-  }, 16000)
+      expect(network).toBe('Network: t')
+      expect(payload).toBe('Payload: da43')
+      expect(protocol).toBe('Protocol: 0')
+      expect(string).toBe('String: t08666')
+      expect(bytes).toBe('Bytes: 00da43')
+    } finally {
+      await browser.close()
+    }
+  }, 20000)
 })
