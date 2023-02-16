25488e3b1b3aaa731a1329b48d61eef6cde23dfb
Emmanuel <emmanuelm41@gmail.com>
2023-02-16 17:03:42 -0300

    feat: add tests for generate mnemonic on cjs and esm packages

--- tests/package/cjs/index.js
+++ tests/package/cjs/index.js
@@ -1,8 +1,14 @@
-const {Address, ProtocolIndicator, Network} = require("@zondax/izari-filecoin-tools")
-const assert = require("assert");
+const { Address, ProtocolIndicator, Network, Wallet } = require('@zondax/izari-filecoin-tools')
+const assert = require('assert')
 
 const addr = Address.fromString('t08666')
 assert(addr.toString() === 't08666')
-assert(addr.toBytes().toString('hex') ==='00da43')
+assert(addr.toBytes().toString('hex') === '00da43')
 assert(addr.protocol === ProtocolIndicator.ID)
 assert(addr.network === Network.Testnet)
+
+for (let i = 0; i < 100; i++) {
+  const data = Wallet.generateMnemonic()
+  const words = data.split(' ')
+  assert(words.length === 24)
+}
--- tests/package/mjs/index.js
+++ tests/package/mjs/index.js
@@ -1,4 +1,4 @@
-import { Address, ProtocolIndicator, Network } from '@zondax/izari-filecoin-tools'
+import { Address, ProtocolIndicator, Network, Wallet } from '@zondax/izari-filecoin-tools'
 import assert from 'assert'
 
 const addr = Address.fromString('t08666')
@@ -6,3 +6,9 @@ assert(addr.toString() === 't08666')
 assert(addr.toBytes().toString('hex') === '00da43')
 assert(addr.protocol === ProtocolIndicator.ID)
 assert(addr.network === Network.Testnet)
+
+for (let i = 0; i < 100; i++) {
+  const data = Wallet.generateMnemonic()
+  const words = data.split(' ')
+  assert(words.length === 24)
+}
