e8c1839f3cdd73d110d174a892589f65884e7d82
Emmanuel <emmanuelm41@gmail.com>
2023-02-18 20:22:36 -0300

    feat: create tests for transactions on jest

---
+++ tests/jest/transaction.test.ts
@@ -0,0 +1,28 @@
+import fs from 'fs'
+import path from 'path'
+
+import { TxTestCase } from '../utils/src/types'
+import { Network, Transaction } from '../../src'
+
+const TXS_VECTOR = '../vectors/txs.json'
+
+describe('Transaction', () => {
+  const vectors = JSON.parse(fs.readFileSync(path.join(__dirname, TXS_VECTOR), 'utf-8')) as TxTestCase[]
+
+  describe('From encoded data', () => {
+    vectors.forEach(({ cbor, tx }, i) => {
+      test('Tx ' + i, () => {
+        const parseTx = Transaction.parse(Network.Mainnet, cbor)
+
+        expect(parseTx.to.toString()).toBe(tx.To)
+        expect(parseTx.from.toString()).toBe(tx.From)
+        expect(parseTx.gasPremium).toBe(tx.GasPremium)
+        expect(parseTx.gasLimit).toBe(tx.GasLimit)
+        expect(parseTx.gasFeeCap).toBe(tx.GasFeeCap)
+        expect(parseTx.method).toBe(tx.Method)
+        expect(parseTx.params).toBe(tx.Params)
+        expect(parseTx.nonce).toBe(tx.Nonce)
+      })
+    })
+  })
+})
