52c164d886fa137e57b7f6691a8c194838abd5ef
Emmanuel <emmanuelm41@gmail.com>
2023-02-23 20:15:52 -0300

    chore: simplify functions

--- src/token/utils.ts
+++ src/token/utils.ts
@@ -1,26 +1,18 @@
 import BN from 'bn.js'
 
 export const trimTrailingZeros = (value: string) => {
-  let i = 0
-  for (i = value.length - 1; i >= 0; i--) {
-    if (value[i] == '.') {
-      i--
-      break
-    } else if (value[i] != '0' || i === 1) break
+  for (let i = value.length - 1; i >= 0; i--) {
+    if (value[i] == '.') return value.substr(0, i)
+    if (value[i] != '0' || i === 1) return value.substr(0, i + 1)
   }
-
-  return value.substr(0, i + 1)
 }
 
 export const bnToString = (value: BN, precision: number) => {
-  let sign = ''
-  if (value.isNeg()) {
-    value = value.abs()
-    sign = '-'
-  }
+  const sign = value.isNeg() ? '-' : ''
 
-  const valueStr = value.toString()
+  const valueStr = value.abs().toString()
   const decimals = valueStr.length - precision
+
   const parsedValue = trimTrailingZeros(
     decimals > 0 ? `${valueStr.substring(0, decimals)}.${valueStr.substring(decimals)}` : `0.${''.padStart(-1 * decimals, '0')}${valueStr}`
   )
