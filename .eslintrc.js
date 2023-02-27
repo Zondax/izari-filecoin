afba21937f374310bf963e925663edecf2648480
Emmanuel <emmanuelm41@gmail.com>
2023-02-27 12:30:13 -0300

    enable tsdoc on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,9 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'eslint-plugin-tsdoc'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking'],
+  rules: {
+    'tsdoc/syntax': 'error',
+  },
 }
