63a926988e3d3a19d65c4765788027ea15f3ed3f
Emmanuel <emmanuelm41@gmail.com>
2023-02-28 11:53:22 -0300

    feat: add test for edge cases

--- tests/jest/address.test.ts
+++ tests/jest/address.test.ts
@@ -1,9 +1,10 @@
 import fs from 'fs'
 import path from 'path'
+import BN from 'bn.js'
 
 import { Address, AddressActor, AddressBls, AddressId, AddressSecp256k1 } from '../../src/address'
 import { Network, ProtocolIndicator } from '../../src/address/constants'
-import { InvalidProtocolIndicator } from '../../src/address/errors'
+import { InvalidPayloadLength, InvalidProtocolIndicator } from '../../src/address/errors'
 
 jest.setTimeout(60 * 1000)
 
@@ -31,6 +32,9 @@ describe('Address', () => {
           expect(addr.protocol).toBe(protocol)
           expect(addr.network).toBe(network)
           expect(addr.payload.toString('hex')).toBe(payload)
+
+          if (Address.isAddressId(addr)) expect(addr.id).toBe(string.slice(2))
+          if (Address.isAddressDelegated(addr)) expect(addr.namespace).toBe(string.slice(2, string.indexOf(network, 1)))
         })
       })
     })
@@ -44,9 +48,13 @@ describe('Address', () => {
 
           expect(addr.toString()).toBe(string)
           expect(addr.toBytes().toString('hex')).toBe(bytes)
+
           expect(addr.protocol).toBe(protocol)
           expect(addr.network).toBe(network)
           expect(addr.payload.toString('hex')).toBe(payload)
+
+          if (Address.isAddressId(addr)) expect(addr.id).toBe(string.slice(2))
+          if (Address.isAddressDelegated(addr)) expect(addr.namespace).toBe(string.slice(2, string.indexOf(network, 1)))
         })
       })
     })
@@ -61,12 +69,46 @@ describe('Address', () => {
           expect(addr.toBytes().toString('hex')).toBe('00da43')
           expect(addr.protocol).toBe(ProtocolIndicator.ID)
           expect(addr.network).toBe(Network.Testnet)
+          expect(Address.isAddressId(addr)).toBeTruthy()
+          if (Address.isAddressId(addr)) expect(addr.id).toBe('8666')
         })
 
         test('Mainnet', async () => {
           const addr = Address.fromString('f08666')
           expect(addr.toString()).toBe('f08666')
           expect(addr.toBytes().toString('hex')).toBe('00da43')
+          expect(addr.protocol).toBe(ProtocolIndicator.ID)
+          expect(addr.network).toBe(Network.Mainnet)
+          expect(Address.isAddressId(addr)).toBeTruthy()
+          if (Address.isAddressId(addr)) expect(addr.id).toBe('8666')
+        })
+
+        test('Exceed max id (super big)', async () => {
+          expect(() => {
+            Address.fromString('t0111111111111111111111111')
+          }).toThrow()
+        })
+
+        test('Exceed max value', async () => {
+          const aboveMax = new BN(2).pow(new BN(63))
+          const addrStr = 'f0' + aboveMax.toString()
+
+          expect(() => {
+            Address.fromString(addrStr)
+          }).toThrow(InvalidPayloadLength)
+
+          expect(() => {
+            new AddressId(Network.Mainnet, aboveMax.toString())
+          }).toThrow(InvalidPayloadLength)
+        })
+
+        test('Max allowed value', async () => {
+          const max = new BN(2).pow(new BN(63)).sub(new BN(1))
+          const addrStr = 'f0' + max.toString()
+
+          const addr = Address.fromString(addrStr)
+          expect(addr.toString()).toBe(addrStr)
+          expect(addr.toBytes().toString('hex')).toBe('00ffffffffffffffff7f')
           expect(addr.protocol).toBe(ProtocolIndicator.ID)
           expect(addr.network).toBe(Network.Mainnet)
         })
@@ -85,6 +127,20 @@ describe('Address', () => {
           const addr = Address.fromBytes(Network.Mainnet, Buffer.from('00da43', 'hex'))
           expect(addr.toString()).toBe('f08666')
           expect(addr.toBytes().toString('hex')).toBe('00da43')
+          expect(addr.protocol).toBe(ProtocolIndicator.ID)
+          expect(addr.network).toBe(Network.Mainnet)
+        })
+
+        test('Exceed max value', async () => {
+          expect(() => {
+            Address.fromBytes(Network.Mainnet, Buffer.from('0080808080808080808001', 'hex'))
+          }).toThrow(InvalidPayloadLength)
+        })
+
+        test('Max allowed value', async () => {
+          const addr = Address.fromBytes(Network.Mainnet, Buffer.from('00ffffffffffffffff7f', 'hex'))
+          expect(addr.toString()).toBe('f09223372036854775807')
+          expect(addr.toBytes().toString('hex')).toBe('00ffffffffffffffff7f')
           expect(addr.protocol).toBe(ProtocolIndicator.ID)
           expect(addr.network).toBe(Network.Mainnet)
         })
