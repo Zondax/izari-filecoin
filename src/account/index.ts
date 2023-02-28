362a546755f5e88924e6a7cb6102372b6c6b2a4f
Emmanuel <emmanuelm41@gmail.com>
2023-02-28 16:22:40 -0300

    feat: add balance method for accounts

--- src/account/index.ts
+++ src/account/index.ts
@@ -1,12 +1,12 @@
 import { Address } from '../address/index.js'
 import { Token } from '../token/index.js'
 import { Transaction } from '../transaction/index.js'
-import { AccountData } from '../types/wallet.js'
+import { AccountData, Cid } from '../artifacts/index.js'
 import { Wallet } from '../wallet/index.js'
 import { RPC } from '../rpc/index.js'
 
 export class Account {
-  static send = async (nodeRpc: RPC, account: AccountData, to: Address, value: Token) => {
+  static send = async (nodeRpc: RPC, account: AccountData, to: Address, value: Token): Promise<Cid> => {
     if (value.isNegative()) throw new Error('value cannot be negative')
     if (value.isZero()) throw new Error('value cannot be zero')
 
@@ -15,6 +15,16 @@ export class Account {
 
     const signature = await Wallet.signTransaction(account, tx)
 
-    return await nodeRpc.broadcastTransaction({ Message: tx.toJSON(), Signature: signature.toJSON() })
+    const response = await nodeRpc.broadcastTransaction({ Message: tx.toJSON(), Signature: signature.toJSON() })
+    if ('error' in response) throw new Error(response.error.message)
+
+    return response.result['/']
+  }
+
+  static getBalance = async (nodeRpc: RPC, account: Pick<AccountData, 'address'>): Promise<Token> => {
+    const response = await nodeRpc.walletBalance(account.address.toString())
+    if ('error' in response) throw new Error(response.error.message)
+
+    return Token.fromAtto(response.result)
   }
 }
