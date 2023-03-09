dd535bb9b488d587fb07d6e4a2b7349555834ff7
Emmanuel <emmanuelm41@gmail.com>
2023-03-09 15:11:23 -0300

    feat: add docs for Transaction class

--- src/transaction/utils.ts
+++ src/transaction/utils.ts
@@ -1,4 +1,11 @@
 import BN from 'bn.js'
+
+/**
+ * Serialize a string number to serialized BigInt per filecoin specification
+ * @param value - string number to serialize
+ * @param base - number base
+ * @returns serialized bigint as buffer
+ */
 export const serializeBigNum = (value: string, base = 10): Buffer => {
   if (value.toString() === '0') return Buffer.from('')
 
