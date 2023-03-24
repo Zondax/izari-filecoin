556140f01b23dadf56415aa091cf03a0dee6eff5
Emmanuel <emmanuelm41@gmail.com>
2023-03-24 14:51:32 -0300

    feat: add deserialize bigint function

--- tests/jest/bignum.test.ts
+++ tests/jest/bignum.test.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 
-import { serializeBigNum } from '../../src/transaction/utils'
+import { serializeBigNum, deserializeBigNum } from '../../src/transaction/utils'
 
 jest.setTimeout(60 * 1000)
 
@@ -21,6 +21,18 @@ describe('Big Numbers', () => {
         const expectedValue = serializeBigNum(numString).toString('hex')
 
         expect(expectedValue).toBe(numSerialized)
+      })
+    })
+  })
+
+  describe('Deserialize', () => {
+    const vectors = JSON.parse(fs.readFileSync(path.join(__dirname, BIGNUMS_VECTOR), 'utf-8')) as BigNumTestCase[]
+
+    vectors.forEach(({ numString, numSerialized }, index) => {
+      test(`Test case ${index}: ${numString}`, () => {
+        const expectedValue = deserializeBigNum(Buffer.from(numSerialized, 'hex'))
+
+        expect(expectedValue).toBe(numString)
       })
     })
   })
