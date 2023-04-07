98d9a3e5d1e692bc52d60e67624298e6aa271b06
Emmanuel <emmanuelm41@gmail.com>
2023-04-07 19:50:54 -0300

    add missing exports (#107)
    * fix TS types version for exports
    * export actors folder too

---
+++ src/actors/index.ts
@@ -0,0 +1,2 @@
+export * from './init/index.js'
+export * from './paymentChannel/index.js'
--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ export * from './token/index.js'
 export * from './account/index.js'
 export * from './artifacts/index.js'
 export * from './payment/index.js'
+export * from './actors/index.js'
 
 export * from './utils/retry.js'
