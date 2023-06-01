fea2be8e03efe19c5461488ac3f83f2d116413b4
Emmanuel <emmanuelm41@gmail.com>
2023-06-01 14:56:36 -0300

    remove safari for now

--- tests/packages/react-app/tests/address.test.ts
+++ tests/packages/react-app/tests/address.test.ts
@@ -3,7 +3,7 @@ import * as playwright from 'playwright'
 const browsers = [
   { name: 'Chromium', instance: playwright.chromium },
   { name: 'Firefox', instance: playwright.firefox },
-  { name: 'Safari', instance: playwright.webkit },
+  //{ name: 'Safari', instance: playwright.webkit },
 ]
 
 describe('Addresses', () => {
